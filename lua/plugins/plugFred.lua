781f3e0aae04f14b125e27c8e543ac99d0b76065
Frederic Tarroux <frederic.tarroux@onera.fr>
2023-09-13 16:36:21 +0200

    diable falsh

--- lua/plugins/plugFred.lua
+++ lua/plugins/plugFred.lua
@@ -704,19 +704,21 @@ return {
   --     require("themery").setup()
   --   end,
   -- },
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
-  },
+  -- {
+  --   "folke/flash.nvim",
+  --
+  --   event = "VeryLazy",
+  --   opts = {},
+  -- -- stylua: ignore
+  -- -- keys = {
+  -- --   { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+  -- --   { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  -- --   { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+  -- --   { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+  -- --   { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  -- -- },
+  -- version = "*",
+  -- },
   -- {
   --   "jackMort/ChatGPT.nvim", -- c'est payant
   --     event = "VeryLazy",
